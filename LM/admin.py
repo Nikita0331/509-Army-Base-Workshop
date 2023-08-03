5d704cb4678edc5101c71784e36100d7fb06b556
Nikita <nikita.nikkyjain@gmail.com>
2023-08-03 17:51:23 +0530

    del

--- LM/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Letter,ViewUser
-
-
-admin.site.register(Letter)
-admin.site.register(ViewUser)
-
-# Register your models here.
--- LM/filters.py
+++
@@ -1,7 +0,0 @@
-import django_filters
-from django_filters import DateFilter
-from .models import * 
-class LetterFilter(django_filters.FilterSet):
-    class Meta:
-        model=Letter
-        fields='__all__'
